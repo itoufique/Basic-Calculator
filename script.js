c9c5d876ab0a496478a6e09687237d818d3921d1
Md Toufique Ansari <mdtoufiqueansari991@gamil.com>
2026-05-18 10:00:37 +0530

    enhanced js by removing repeated functions

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
 let currentval='';
 document.querySelector('.display-box').value=currentval;
-function one(){
-    currentval=currentval+'1'
-document.querySelector('.display-box').value=currentval;
-}
-function two(){
-    currentval=currentval+'2'
-document.querySelector('.display-box').value=currentval;
-}
-function three(){
-    currentval=currentval+'3'
-document.querySelector('.display-box').value=currentval;
-}
-function four(){
-    currentval=currentval+'4'
-document.querySelector('.display-box').value=currentval;
-}
-function five(){
-    currentval=currentval+'5'
-document.querySelector('.display-box').value=currentval;
-}
-function six(){
-    currentval=currentval+'6'
-document.querySelector('.display-box').value=currentval;
-}
-function seven(){
-    currentval=currentval+'7'
-document.querySelector('.display-box').value=currentval;
-}
-function eight(){
-    currentval=currentval+'8'
-document.querySelector('.display-box').value=currentval;
-}
-function nine(){
-    currentval=currentval+'9'
+function toPerform(value){
+    currentval=currentval+value
 document.querySelector('.display-box').value=currentval;
 }
 function equalto(){
@@ -42,29 +10,5 @@ document.querySelector('.display-box').value=currentval;
 }
 function clearDisplay(){
     currentval=''
-document.querySelector('.display-box').value=currentval;
-}
-function add(){
-    currentval=currentval+'+'
-document.querySelector('.display-box').value=currentval;
-}
-function sub(){
-    currentval=currentval+'-'
-document.querySelector('.display-box').value=currentval;
-}
-function multiply(){
-    currentval=currentval+'*'
-document.querySelector('.display-box').value=currentval;
-}
-function devide(){
-    currentval=currentval+'/'
-document.querySelector('.display-box').value=currentval;
-}
-function dot(){
-    currentval=currentval+'.'
-document.querySelector('.display-box').value=currentval;
-}
-function zero(){
-    currentval=currentval+'0'
 document.querySelector('.display-box').value=currentval;
 }
